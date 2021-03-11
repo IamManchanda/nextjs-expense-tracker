90d6208862d14b7418e08aafa411085e4e0ec61d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-11 12:49:33 +0530

    Starting work on Next.js Expense Tracker App

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+function PageIndex() {
+  return (
+    <div>
+      <Head>
+        <title>Nextjs Expense Tracker App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default PageIndex;
